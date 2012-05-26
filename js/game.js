600c5a948480bfbbd7aa7dedc30eec98d30d5911
Pablo Novas <pjnovas@gmail.com>
2012-05-26 05:17:31 -0300

    updated first scene of the school

--- js/game.js
+++ js/game.js
@@ -3,20 +3,32 @@
 	
 	Pac.repository	
 		.addResources({
-			'school':'images/school_hallway.jpg'
+			'school_hallway':'images/school_hallway.jpg',
+			'school_front':'images/school_front.png'
 		});
 
-	var school = new Pac.Scene('schoolHallway', 'The hallway of my school', 'school');
+	var school = new Pac.Scene('schoolFront', 'Outside of my school', 'school_front');
 	
 	Pac.config({
-		commandBarEnabled: true,
-		text: {font: '50px saint', x: 20},
-		textCmd: {font: '20px saint-serif'}
+		commandBar: {
+			enabled: true,
+			color: 'rgba(0,0,0,0.7)',
+			height: 30
+		},
+		text: {
+			font: '50px saint', 
+			x: 20
+		},
+		textCmd: {
+			font: '20px saint-serif',
+			color: 'yellow',
+			colorSelect: 'white'
+		}
 	});
 	
 	Pac.init('canvas').addScene(school);
 	
-	Pac.changeToScene('schoolHallway');
+	Pac.changeToScene('schoolFront');
 	
 	Pac.repository.on('complete', function(){
 		Pac.start();		
